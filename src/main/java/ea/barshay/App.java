de9e66b12e76af2b083783993592a9c301a45e9f
barshaye <barshaye@gmail.com>
2025-05-19 08:27:26 +0300

    Logback added

--- src/main/java/ea/barshay/App.java
+++ src/main/java/ea/barshay/App.java
@@ -1,5 +1,12 @@
 package ea.barshay;
 
+import io.opentelemetry.sdk.trace.IdGenerator;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -7,8 +14,9 @@ import org.springframework.kafka.annotation.EnableKafka;
 @EnableKafka
 @SpringBootApplication
 public class App {
-
+  private static final Logger LOG = LoggerFactory.getLogger(App.class);
   public static void main(String[] args) {
-    SpringApplication.run(App.class, args);
+      SpringApplication.run(App.class, args);
+    LOG.info("Принято сообщение в систему Новый блок принятия решений {}", LocalDateTime.now());
   }
 }
--- src/test/java/ea/barshay/E2ELoggerTest.java
+++ src/test/java/ea/barshay/E2ELoggerTest.java
@@ -1,4 +1,54 @@
 package ea.barshay;
 
+import io.opentelemetry.sdk.trace.IdGenerator;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.slf4j.event.Level;
+
 public class E2ELoggerTest {
+  @BeforeAll
+  static void setup() {
+    System.setProperty("logging.config", "classpath:logback-spring.xml");
+  }
+
+  @Test
+  void testLogging() {
+    try {
+      String [] identifiers =  "00-12345678901234567890123456789012-1234567890123456-01".split("-");
+      IdGenerator idGenerator = IdGenerator.random();
+      MDC.put("trace_id", identifiers[1]);
+      MDC.put("span_id", idGenerator.generateSpanId());
+      MDC.put("par_span_id", identifiers[2]);
+      MDC.put("step","7");
+      MDC.put("prc_name", "SendForm");
+      MDC.put("prc_id","abcdef1234567890");
+      MDC.put("date",  DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(LocalDateTime.now()));
+      MDC.put("msg_id", "");
+      MDC.put("service", "bprGetForm");
+      MDC.put("step_name", "Получение заявки в БПР");
+      MDC.put("system", "BPR");
+      MDC.put("type",  "Request");
+      MDC.put("host", "sas-rtdm1-test");
+      MDC.put("error", null);
+
+      //log(LoggerFactory.getLogger("E2E_ELASTIC"));
+      log(LoggerFactory.getLogger("FILEROLLING"));
+      //log(LoggerFactory.getLogger("CONSOLE"));
+    } finally {
+      MDC.clear();
+    }
+  }
+  private void log(Logger logger) {
+    Assertions.assertNotNull(logger);
+    Assertions.assertTrue(logger.isEnabledForLevel(Level.DEBUG));
+    Assertions.assertFalse(logger.isEnabledForLevel(Level.DEBUG));
+    logger.info("Принято сообщение в систему Новый блок принятия решений");
+
+  }
 }
